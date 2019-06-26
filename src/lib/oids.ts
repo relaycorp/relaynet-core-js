8da9c72f4834a555c17bba39123d2dda2d7a6694
Gus Narea <gus@relaycorp.tech>
2019-06-26 16:58:11 +0100

    Fix formatting issue

--- src/lib/oids.ts
+++ src/lib/oids.ts
@@ -7,4 +7,4 @@ export const COMMON_NAME = '2.5.4.3';
 export const AUTHORITY_KEY_ID = '2.5.29.35';
 export const SUBJECT_KEY_ID = '2.5.29.14';
 
-export const CMS_SIGNED_DATA = "1.2.840.113549.1.7.2";
+export const CMS_SIGNED_DATA = '1.2.840.113549.1.7.2';
