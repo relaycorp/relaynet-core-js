1872b4b22af7ccd2ed1a467a65aaf9e7ab0da3ef
gnarea <gnarea@users.noreply.github.com>
2020-01-06 19:29:25 +0000

    Automated deployment: Mon Jan  6 19:29:25 UTC 2020 7134235f5e1a48bb69dcc206b3756c986b1b1885

--- assets/js/database.js
+++ assets/js/database.js
@@ -6,7 +6,7 @@ window.database = {
       "category": "",
       "content": "Relaynet SDKThis library implements the core of Relaynet and is meant to be used by anyone supporting Relaynet.InstallUseTypeScript type declarations are included with this library.Specs supported  Cryptographic algorithms supportedSupportIf you have any questions or comments, you can find us on Gitter or create an issue on the GitHub project.UpdatesReleases are automatically published on GitHub and NPM, and the changelog can be found on GitHub.",
       "url": "/",
-      "href": "/"
+      "href": "/relaynet-core-js/"
     }
     ,
   
