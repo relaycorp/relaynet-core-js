f85860a6c9f0eaeceeeadb3bcee5f9a0deca905d
gnarea <gnarea@users.noreply.github.com>
2020-01-06 16:40:33 +0000

    Automated deployment: Mon Jan  6 16:40:33 UTC 2020 eac7390c3ea6269a25cc95b95d11d0bc94e57012

---
+++ api/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"interfaces/payload.html#serialize.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"Payload.serialize"}]};
