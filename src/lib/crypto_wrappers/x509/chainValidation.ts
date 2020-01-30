27b2a8944ba424b40e78f9678606e9e597438808
Gus Narea <gnarea@users.noreply.github.com>
2020-01-30 15:04:57 +0000

    Add missing signature docs

--- src/lib/crypto_wrappers/x509/chainValidation.ts
+++ src/lib/crypto_wrappers/x509/chainValidation.ts
@@ -5,6 +5,13 @@ import Certificate from './Certificate';
 
 export class UntrustedCertificateError extends RelaynetError {}
 
+/**
+ * Report whether a certificate can be trusted.
+ *
+ * @param certificate The certificate to be checked
+ * @param intermediateCaCertificates The alleged chain for the certificate
+ * @param trustedCertificates The collection of certificates that are actually trusted
+ */
 export async function validateCertificateTrust(
   certificate: Certificate,
   intermediateCaCertificates: readonly Certificate[],
