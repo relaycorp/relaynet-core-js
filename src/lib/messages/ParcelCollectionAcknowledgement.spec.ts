141c542927deb06e05132fc80b19f5a7702c4eb3
Gustavo Narea <gnarea@users.noreply.github.com>
2020-04-25 17:31:48 +0100

    Write tests

---
+++ src/lib/messages/ParcelCollectionAcknowledgement.spec.ts
@@ -0,0 +1,19 @@
+describe('ParcelCollectionAcknowledgement', () => {
+  describe('serialize', () => {
+    test.todo('Serialization should start with format signature');
+
+    test.todo('ACKs should be encoded as a DER SET');
+
+    test.todo('First item in ACK sequence should be private address of origin endpoint');
+
+    test.todo('Second item in ACK sequence should be parcel id');
+  });
+
+  describe('deserialize', () => {
+    test.todo('Serialization should start with format signature');
+
+    test.todo('ACKs should be encoded as a DER SET');
+
+    test.todo('Each ACK should be a two-item sequence of VisibleStrings');
+  });
+});
