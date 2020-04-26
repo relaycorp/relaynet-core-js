eb60ea3f56b8ebfb0c53387f2aca9cea2573dd92
Gus Narea <gnarea@users.noreply.github.com>
2020-04-26 20:04:38 +0100

    fix(messages): Make PCA a top-level message instead of a collection

--- src/lib/messages/ParcelCollectionAck.spec.ts
+++ src/lib/messages/ParcelCollectionAck.spec.ts
@@ -3,92 +3,60 @@ import * as asn1js from 'asn1js';
 import { arrayBufferFrom } from '../_test_utils';
 import { derDeserialize } from '../crypto_wrappers/_utils';
 import InvalidMessageError from './InvalidMessageError';
-import { ParcelCollectionAck, ParcelCollectionAckSet } from './ParcelCollectionAck';
+import { ParcelCollectionAck } from './ParcelCollectionAck';
 
 describe('ParcelCollectionAck', () => {
-  const ACK: ParcelCollectionAck = {
-    parcelId: 'the-parcel-id',
-    recipientEndpointAddress: 'https://example.com',
-    senderEndpointPrivateAddress: '0deadbeef',
-  };
+  const SENDER_ENDPOINT_PRIVATE_ADDRESS = '0deadbeef';
+  const RECIPIENT_ENDPOINT_ADDRESS = 'https://example.com';
+  const PARCEL_ID = 'the-parcel-id';
 
   describe('serialize', () => {
     test('Serialization should start with format signature', () => {
-      const ackSet = new ParcelCollectionAckSet(new Set([ACK]));
+      const pca = new ParcelCollectionAck(
+        SENDER_ENDPOINT_PRIVATE_ADDRESS,
+        RECIPIENT_ENDPOINT_ADDRESS,
+        PARCEL_ID,
+      );
 
-      const ackSetSerialized = ackSet.serialize();
+      const pcaSerialized = pca.serialize();
 
       const expectedFormatSignature = Buffer.concat([
         Buffer.from('Relaynet'),
         Buffer.from([0x51, 0x00]),
       ]);
-      expect(Buffer.from(ackSetSerialized).slice(0, 10)).toEqual(expectedFormatSignature);
+      expect(Buffer.from(pcaSerialized).slice(0, 10)).toEqual(expectedFormatSignature);
     });
 
-    test('ACKs should be encoded as a DER SET', () => {
-      const ackSet = new ParcelCollectionAckSet(new Set([ACK]));
+    test('An ACK should be serialized as a 3-item sequence', () => {
+      const pca = new ParcelCollectionAck(
+        SENDER_ENDPOINT_PRIVATE_ADDRESS,
+        RECIPIENT_ENDPOINT_ADDRESS,
+        PARCEL_ID,
+      );
 
-      const derSetSerialized = skipFormatSignatureFromSerialization(ackSet.serialize());
+      const pcaBlock = parsePCA(pca.serialize());
 
-      expect(derDeserialize(derSetSerialized)).toBeInstanceOf(asn1js.Set);
+      expect(pcaBlock).toBeInstanceOf(asn1js.Sequence);
+      const pcaSequence = pcaBlock as asn1js.Sequence;
+      expect(pcaSequence.valueBlock.value).toHaveLength(3);
+      expect((pcaSequence.valueBlock.value[0] as asn1js.VisibleString).valueBlock.value).toEqual(
+        SENDER_ENDPOINT_PRIVATE_ADDRESS,
+      );
+      expect((pcaSequence.valueBlock.value[1] as asn1js.VisibleString).valueBlock.value).toEqual(
+        RECIPIENT_ENDPOINT_ADDRESS,
+      );
+      expect((pcaSequence.valueBlock.value[2] as asn1js.VisibleString).valueBlock.value).toEqual(
+        PARCEL_ID,
+      );
     });
 
-    test('ACKs may be empty', () => {
-      const ackSet = new ParcelCollectionAckSet(new Set([]));
-
-      const derSet = parseAckSet(ackSet.serialize());
-
-      expect(derSet.valueBlock.value).toHaveLength(0);
-    });
-
-    test('An ACK should be a 3-item sequence', () => {
-      const ackSet = new ParcelCollectionAckSet(new Set([ACK]));
-
-      const derSet = parseAckSet(ackSet.serialize());
-
-      const ack = derSet.valueBlock.value[0];
-      expect(ack).toBeInstanceOf(asn1js.Sequence);
-      expect((ack as asn1js.Sequence).valueBlock.value).toHaveLength(3);
-    });
-
-    test('First item in ACK set should be private address of sender endpoint', () => {
-      const ackSet = new ParcelCollectionAckSet(new Set([ACK]));
-
-      const derSet = parseAckSet(ackSet.serialize());
-
-      const ack = derSet.valueBlock.value[0] as asn1js.Sequence;
-      const addressBlock = ack.valueBlock.value[0] as asn1js.VisibleString;
-      expect(addressBlock.valueBlock.value).toEqual(ACK.senderEndpointPrivateAddress);
-    });
-
-    test('Second item in ACK set should be address of recipient endpoint', () => {
-      const ackSet = new ParcelCollectionAckSet(new Set([ACK]));
-
-      const derSet = parseAckSet(ackSet.serialize());
-
-      const ack = derSet.valueBlock.value[0] as asn1js.Sequence;
-      const addressBlock = ack.valueBlock.value[1] as asn1js.VisibleString;
-      expect(addressBlock.valueBlock.value).toEqual(ACK.recipientEndpointAddress);
-    });
-
-    test('Third item in ACK set should be parcel id', () => {
-      const ackSet = new ParcelCollectionAckSet(new Set([ACK]));
-
-      const derSet = parseAckSet(ackSet.serialize());
-
-      const ack = derSet.valueBlock.value[0] as asn1js.Sequence;
-      const parcelIdBlock = ack.valueBlock.value[2] as asn1js.VisibleString;
-      expect(parcelIdBlock.valueBlock.value).toEqual(ACK.parcelId);
-    });
-
-    function skipFormatSignatureFromSerialization(ackSetSerialized: ArrayBuffer): ArrayBuffer {
-      return ackSetSerialized.slice(10);
+    function skipFormatSignatureFromSerialization(pcaSerialized: ArrayBuffer): ArrayBuffer {
+      return pcaSerialized.slice(10);
     }
 
-    function parseAckSet(ackSetSerialized: ArrayBuffer): asn1js.Set {
-      const derSetSerialized = skipFormatSignatureFromSerialization(ackSetSerialized);
-
-      return derDeserialize(derSetSerialized) as asn1js.Set;
+    function parsePCA(pcaSerialized: ArrayBuffer): asn1js.Sequence {
+      const derSequenceSerialized = skipFormatSignatureFromSerialization(pcaSerialized);
+      return derDeserialize(derSequenceSerialized) as asn1js.Sequence;
     }
   });
 
@@ -96,101 +64,67 @@ describe('ParcelCollectionAck', () => {
     test('Serialization should start with format signature', () => {
       const invalidSerialization = arrayBufferFrom('RelaynetA0');
 
-      expect(() => ParcelCollectionAckSet.deserialize(invalidSerialization)).toThrowWithMessage(
+      expect(() => ParcelCollectionAck.deserialize(invalidSerialization)).toThrowWithMessage(
         InvalidMessageError,
-        'Format signature should be that of a PCA set',
-      );
-    });
-
-    test('ACKs should be encoded as a DER SET', () => {
-      // Pass an ASN.1 NULL instead of a SET
-      const invalidSerialization = arrayBufferFrom([
-        ...ParcelCollectionAckSet.FORMAT_SIGNATURE,
-        ...Buffer.from(new asn1js.Null().toBER(false)),
-      ]);
-
-      expect(() => ParcelCollectionAckSet.deserialize(invalidSerialization)).toThrowWithMessage(
-        InvalidMessageError,
-        'PCA set did not meet required structure',
+        'Format signature should be that of a PCA',
       );
     });
 
     test('ACK should be refused if it has fewer than 3 items', () => {
       // Pass an ACK with 2 VisibleStrings instead of 3
       const invalidSerialization = arrayBufferFrom([
-        ...ParcelCollectionAckSet.FORMAT_SIGNATURE,
+        ...ParcelCollectionAck.FORMAT_SIGNATURE,
         ...Buffer.from(
-          new asn1js.Set({
-            value: [
-              new asn1js.Sequence({
-                value: [new asn1js.VisibleString(), new asn1js.VisibleString()],
-              } as any),
-            ],
+          new asn1js.Sequence({
+            value: [new asn1js.VisibleString(), new asn1js.VisibleString()],
           } as any).toBER(false),
         ),
       ]);
 
-      expect(() => ParcelCollectionAckSet.deserialize(invalidSerialization)).toThrowWithMessage(
+      expect(() => ParcelCollectionAck.deserialize(invalidSerialization)).toThrowWithMessage(
         InvalidMessageError,
-        'PCA set did not meet required structure',
+        'PCA did not meet required structure',
       );
     });
 
     test('Each ACK should be a three-item sequence of VisibleStrings', () => {
       const invalidSerialization = arrayBufferFrom([
-        ...ParcelCollectionAckSet.FORMAT_SIGNATURE,
+        ...ParcelCollectionAck.FORMAT_SIGNATURE,
         ...Buffer.from(
-          new asn1js.Set({
+          new asn1js.Sequence({
             value: [
-              new asn1js.Sequence({
-                value: [
-                  new asn1js.VisibleString(),
-                  new asn1js.Integer({ value: 42 }),
-                  new asn1js.VisibleString(),
-                ],
-              } as any),
+              new asn1js.VisibleString(),
+              new asn1js.Integer({ value: 42 }),
+              new asn1js.VisibleString(),
             ],
           } as any).toBER(false),
         ),
       ]);
 
-      expect(() => ParcelCollectionAckSet.deserialize(invalidSerialization)).toThrowWithMessage(
+      expect(() => ParcelCollectionAck.deserialize(invalidSerialization)).toThrowWithMessage(
         InvalidMessageError,
-        'PCA set did not meet required structure',
+        'PCA did not meet required structure',
       );
     });
 
     test('A new instance should be returned if serialization is valid', () => {
       const invalidSerialization = arrayBufferFrom([
-        ...ParcelCollectionAckSet.FORMAT_SIGNATURE,
+        ...ParcelCollectionAck.FORMAT_SIGNATURE,
         ...Buffer.from(
-          new asn1js.Set({
+          new asn1js.Sequence({
             value: [
-              new asn1js.Sequence({
-                value: [
-                  new asn1js.VisibleString({ value: ACK.senderEndpointPrivateAddress }),
-                  new asn1js.VisibleString({ value: ACK.recipientEndpointAddress }),
-                  new asn1js.VisibleString({ value: ACK.parcelId }),
-                ],
-              } as any),
+              new asn1js.VisibleString({ value: SENDER_ENDPOINT_PRIVATE_ADDRESS }),
+              new asn1js.VisibleString({ value: RECIPIENT_ENDPOINT_ADDRESS }),
+              new asn1js.VisibleString({ value: PARCEL_ID }),
             ],
           } as any).toBER(false),
         ),
       ]);
 
-      const pcaSet = ParcelCollectionAckSet.deserialize(invalidSerialization);
-      expect(pcaSet.ackSet).toHaveProperty('size', 1);
-      expect(Array.from(pcaSet.ackSet)[0]).toEqual(ACK);
-    });
-
-    test('PCA set may be empty', () => {
-      const invalidSerialization = arrayBufferFrom([
-        ...ParcelCollectionAckSet.FORMAT_SIGNATURE,
-        ...Buffer.from(new asn1js.Set({ value: [] } as any).toBER(false)),
-      ]);
-
-      const pcaSet = ParcelCollectionAckSet.deserialize(invalidSerialization);
-      expect(pcaSet.ackSet).toHaveProperty('size', 0);
+      const pca = ParcelCollectionAck.deserialize(invalidSerialization);
+      expect(pca.senderEndpointPrivateAddress).toEqual(SENDER_ENDPOINT_PRIVATE_ADDRESS);
+      expect(pca.recipientEndpointAddress).toEqual(RECIPIENT_ENDPOINT_ADDRESS);
+      expect(pca.parcelId).toEqual(PARCEL_ID);
     });
   });
 });
--- src/lib/messages/ParcelCollectionAck.ts
+++ src/lib/messages/ParcelCollectionAck.ts
@@ -3,82 +3,63 @@ import * as asn1js from 'asn1js';
 import { generateFormatSignature } from './formatSignature';
 import InvalidMessageError from './InvalidMessageError';
 
-export interface ParcelCollectionAck {
-  readonly senderEndpointPrivateAddress: string;
-  readonly recipientEndpointAddress: string;
-  readonly parcelId: string;
-}
-
-export class ParcelCollectionAckSet {
+export class ParcelCollectionAck {
   public static readonly FORMAT_SIGNATURE = generateFormatSignature(0x51, 0);
 
-  public static deserialize(pcaSerialized: ArrayBuffer): ParcelCollectionAckSet {
+  public static deserialize(pcaSerialized: ArrayBuffer): ParcelCollectionAck {
     const formatSignature = Buffer.from(
-      pcaSerialized.slice(0, ParcelCollectionAckSet.FORMAT_SIGNATURE.byteLength),
+      pcaSerialized.slice(0, ParcelCollectionAck.FORMAT_SIGNATURE.byteLength),
     );
-    if (!formatSignature.equals(ParcelCollectionAckSet.FORMAT_SIGNATURE)) {
-      throw new InvalidMessageError('Format signature should be that of a PCA set');
+    if (!formatSignature.equals(ParcelCollectionAck.FORMAT_SIGNATURE)) {
+      throw new InvalidMessageError('Format signature should be that of a PCA');
     }
 
-    const pcaSetSerialized = pcaSerialized.slice(10);
-    const result = asn1js.verifySchema(pcaSetSerialized, ParcelCollectionAckSet.SCHEMA);
+    const pcaSequenceSerialized = pcaSerialized.slice(10);
+    const result = asn1js.verifySchema(pcaSequenceSerialized, ParcelCollectionAck.SCHEMA);
     if (!result.verified) {
-      throw new InvalidMessageError('PCA set did not meet required structure');
+      throw new InvalidMessageError('PCA did not meet required structure');
     }
 
-    const ackBlocks: readonly asn1js.Set[] = (result.result as any).ack_set ?? [];
-    const acks: readonly ParcelCollectionAck[] = ackBlocks.map(ackBlock => ({
-      parcelId: (ackBlock.valueBlock.value[2] as asn1js.VisibleString).valueBlock.value,
-      recipientEndpointAddress: (ackBlock.valueBlock.value[1] as asn1js.VisibleString).valueBlock
-        .value,
-      senderEndpointPrivateAddress: (ackBlock.valueBlock.value[0] as asn1js.VisibleString)
-        .valueBlock.value,
-    }));
-    return new ParcelCollectionAckSet(new Set(acks));
+    const pcaBlock: any = (result.result as any).ParcelCollectionAck;
+    return new ParcelCollectionAck(
+      (pcaBlock.senderEndpointPrivateAddress as asn1js.VisibleString).valueBlock.value,
+      (pcaBlock.recipientEndpointAddress as asn1js.VisibleString).valueBlock.value,
+      (pcaBlock.parcelId as asn1js.VisibleString).valueBlock.value,
+    );
   }
 
-  private static readonly SCHEMA = new asn1js.Set({
-    name: 'ParcelCollectionAckSet',
+  private static readonly SCHEMA = new asn1js.Sequence({
+    name: 'ParcelCollectionAck',
     value: [
-      new asn1js.Repeated({
-        name: 'ack_set',
-        value: new asn1js.Sequence({
-          name: 'ack',
-          value: [
-            new asn1js.VisibleString({
-              name: 'senderEndpointPrivateAddress',
-              optional: false,
-            } as any),
-            new asn1js.VisibleString({ name: 'recipientEndpointAddress', optional: false } as any),
-            new asn1js.VisibleString({ name: 'parcelId', optional: false } as any),
-          ],
-        } as any),
-      } as any),
+      new asn1js.VisibleString({ name: 'senderEndpointPrivateAddress', optional: false } as any),
+      new asn1js.VisibleString({ name: 'recipientEndpointAddress', optional: false } as any),
+      new asn1js.VisibleString({ name: 'parcelId', optional: false } as any),
     ],
   } as any);
 
-  constructor(public readonly ackSet: ReadonlySet<ParcelCollectionAck>) {}
+  constructor(
+    public readonly senderEndpointPrivateAddress: string,
+    public readonly recipientEndpointAddress: string,
+    public readonly parcelId: string,
+  ) {}
 
   public serialize(): ArrayBuffer {
-    const ackSequences = [...this.ackSet].map(
-      ack =>
-        new asn1js.Sequence({
-          value: [
-            new asn1js.VisibleString({ value: ack.senderEndpointPrivateAddress }),
-            new asn1js.VisibleString({ value: ack.recipientEndpointAddress }),
-            new asn1js.VisibleString({ value: ack.parcelId }),
-          ],
-        } as any),
-    );
-    const ackSetSerialized = new asn1js.Set({ value: ackSequences } as any).toBER(false);
+    const ackBlock = new asn1js.Sequence({
+      value: [
+        new asn1js.VisibleString({ value: this.senderEndpointPrivateAddress }),
+        new asn1js.VisibleString({ value: this.recipientEndpointAddress }),
+        new asn1js.VisibleString({ value: this.parcelId }),
+      ],
+    } as any);
+    const ackSerialized = ackBlock.toBER(false);
     const serialization = new ArrayBuffer(
-      ParcelCollectionAckSet.FORMAT_SIGNATURE.byteLength + ackSetSerialized.byteLength,
+      ParcelCollectionAck.FORMAT_SIGNATURE.byteLength + ackSerialized.byteLength,
     );
     const serializationView = new Uint8Array(serialization);
-    serializationView.set(ParcelCollectionAckSet.FORMAT_SIGNATURE, 0);
+    serializationView.set(ParcelCollectionAck.FORMAT_SIGNATURE, 0);
     serializationView.set(
-      new Uint8Array(ackSetSerialized),
-      ParcelCollectionAckSet.FORMAT_SIGNATURE.byteLength,
+      new Uint8Array(ackSerialized),
+      ParcelCollectionAck.FORMAT_SIGNATURE.byteLength,
     );
     return serialization;
   }
