b39289c460791468fb7e358b0be8d3d7a8ec103b
Gus Narea <gnarea@users.noreply.github.com>
2020-08-25 17:03:45 +0100

    Implement raw RSA signatures

---
+++ src/lib/crypto_wrappers/rsaSigning.ts
@@ -0,0 +1,21 @@
+import * as utils from './_utils';
+
+const rsaPssParams = {
+  hash: { name: 'SHA-256' },
+  name: 'RSA-PSS',
+  saltLength: 32,
+};
+
+const pkijsCrypto = utils.getPkijsCrypto();
+
+export async function sign(plaintext: ArrayBuffer, privateKey: CryptoKey): Promise<ArrayBuffer> {
+  return pkijsCrypto.sign(rsaPssParams, privateKey, plaintext);
+}
+
+export async function verify(
+  signature: ArrayBuffer,
+  publicKey: CryptoKey,
+  expectedPlaintext: ArrayBuffer,
+): Promise<boolean> {
+  return pkijsCrypto.verify(rsaPssParams, publicKey, signature, expectedPlaintext);
+}
