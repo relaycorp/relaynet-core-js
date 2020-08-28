08d380045299c5a0603faf96f5b4ce57c209fdfa
Gus Narea <gnarea@users.noreply.github.com>
2020-08-28 12:07:16 +0100

    fix: Rename "client registration" to "private node registration" (#146)

--- src/index.ts
+++ src/index.ts
@@ -57,9 +57,9 @@ export { default as RAMFValidationError } from './lib/ramf/RAMFValidationError';
 
 // Control messages
 export * from './lib/messages/ParcelCollectionAck';
-export { ClientRegistrationAuthorization } from './lib/messages/bindings/pdc/ClientRegistrationAuthorization';
-export { ClientRegistration } from './lib/messages/bindings/pdc/ClientRegistration';
-export { ClientRegistrationRequest } from './lib/messages/bindings/pdc/ClientRegistrationRequest';
+export { PrivateNodeRegistrationAuthorization } from './lib/messages/bindings/pdc/PrivateNodeRegistrationAuthorization';
+export { PrivateNodeRegistration } from './lib/messages/bindings/pdc/PrivateNodeRegistration';
+export { PrivateNodeRegistrationRequest } from './lib/messages/bindings/pdc/PrivateNodeRegistrationRequest';
 // Nodes
 export { Gateway, CargoMessageStream } from './lib/nodes/gateway';
 
---
+++ src/lib/messages/bindings/pdc/PrivateNodeRegistration.ts
@@ -0,0 +1,53 @@
+import { OctetString, verifySchema } from 'asn1js';
+import { makeSequenceSchema, serializeSequence } from '../../../asn1';
+import Certificate from '../../../crypto_wrappers/x509/Certificate';
+import InvalidMessageError from '../../InvalidMessageError';
+
+export class PrivateNodeRegistration {
+  public static deserialize(serialization: ArrayBuffer): PrivateNodeRegistration {
+    const result = verifySchema(serialization, PrivateNodeRegistration.SCHEMA);
+    if (!result.verified) {
+      throw new InvalidMessageError('Serialization is not a valid PrivateNodeRegistration');
+    }
+
+    const registrationASN1 = (result.result as any).PrivateNodeRegistration;
+
+    // tslint:disable-next-line:no-let
+    let privateNodeCertificate: Certificate;
+    try {
+      privateNodeCertificate = Certificate.deserialize(
+        registrationASN1.privateNodeCertificate.valueBlock.valueHex,
+      );
+    } catch (exc) {
+      throw new InvalidMessageError(exc, 'Private node certificate is invalid');
+    }
+
+    // tslint:disable-next-line:no-let
+    let gatewayCertificate: Certificate;
+    try {
+      gatewayCertificate = Certificate.deserialize(
+        registrationASN1.gatewayCertificate.valueBlock.valueHex,
+      );
+    } catch (exc) {
+      throw new InvalidMessageError(exc, 'Gateway certificate is invalid');
+    }
+    return new PrivateNodeRegistration(privateNodeCertificate, gatewayCertificate);
+  }
+
+  private static readonly SCHEMA = makeSequenceSchema('PrivateNodeRegistration', [
+    'privateNodeCertificate',
+    'gatewayCertificate',
+  ]);
+
+  constructor(
+    public readonly privateNodeCertificate: Certificate,
+    public readonly gatewayCertificate: Certificate,
+  ) {}
+
+  public serialize(): ArrayBuffer {
+    return serializeSequence(
+      new OctetString({ valueHex: this.privateNodeCertificate.serialize() }),
+      new OctetString({ valueHex: this.gatewayCertificate.serialize() }),
+    );
+  }
+}
