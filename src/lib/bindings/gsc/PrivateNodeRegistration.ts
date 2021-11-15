566f320ea9acd7ed0b9e64b0555773c852c30943
Gus Narea <gnarea@users.noreply.github.com>
2021-11-15 16:02:58 +0000

    fix(PrivateNodeRegistration): Include session key
    Needed in https://github.com/relaycorp/relaynet-internet-gateway/pull/668

--- src/lib/bindings/gsc/PrivateNodeRegistration.ts
+++ src/lib/bindings/gsc/PrivateNodeRegistration.ts
@@ -1,19 +1,20 @@
-import { OctetString, Primitive, verifySchema } from 'asn1js';
+import { Constructed, OctetString, Primitive, verifySchema } from 'asn1js';
+import bufferToArray from 'buffer-to-arraybuffer';
 
 import { makeHeterogeneousSequenceSchema, makeImplicitlyTaggedSequence } from '../../asn1';
+import { derDeserializeECDHPublicKey, derSerializePublicKey } from '../../crypto_wrappers/keys';
 import Certificate from '../../crypto_wrappers/x509/Certificate';
 import InvalidMessageError from '../../messages/InvalidMessageError';
+import { SessionKey } from '../../SessionKey';
 
 export class PrivateNodeRegistration {
-  public static deserialize(serialization: ArrayBuffer): PrivateNodeRegistration {
+  public static async deserialize(serialization: ArrayBuffer): Promise<PrivateNodeRegistration> {
     const result = verifySchema(serialization, PrivateNodeRegistration.SCHEMA);
     if (!result.verified) {
       throw new InvalidMessageError('Serialization is not a valid PrivateNodeRegistration');
     }
-
     const registrationASN1 = (result.result as any).PrivateNodeRegistration;
 
-    // tslint:disable-next-line:no-let
     let privateNodeCertificate: Certificate;
     try {
       privateNodeCertificate = Certificate.deserialize(
@@ -23,32 +24,77 @@ export class PrivateNodeRegistration {
       throw new InvalidMessageError(exc, 'Private node certificate is invalid');
     }
 
-    // tslint:disable-next-line:no-let
     let gatewayCertificate: Certificate;
     try {
       gatewayCertificate = Certificate.deserialize(
         registrationASN1.gatewayCertificate.valueBlock.valueHex,
       );
-    } catch (exc) {
-      throw new InvalidMessageError(exc, 'Gateway certificate is invalid');
+    } catch (err) {
+      throw new InvalidMessageError(err, 'Gateway certificate is invalid');
     }
-    return new PrivateNodeRegistration(privateNodeCertificate, gatewayCertificate);
+
+    const sessionKey = await deserializeSessionKey(registrationASN1.sessionKey);
+
+    return new PrivateNodeRegistration(privateNodeCertificate, gatewayCertificate, sessionKey);
   }
 
   private static readonly SCHEMA = makeHeterogeneousSequenceSchema('PrivateNodeRegistration', [
     new Primitive({ name: 'privateNodeCertificate' }),
     new Primitive({ name: 'gatewayCertificate' }),
+    new Constructed({
+      name: 'sessionKey',
+      optional: true,
+      value: [
+        new Primitive({ idBlock: { tagClass: 3, tagNumber: 0 } } as any),
+        new Primitive({ idBlock: { tagClass: 3, tagNumber: 1 } } as any),
+      ],
+    } as any),
   ]);
 
   constructor(
     public readonly privateNodeCertificate: Certificate,
     public readonly gatewayCertificate: Certificate,
+    public readonly sessionKey: SessionKey | null = null,
   ) {}
 
-  public serialize(): ArrayBuffer {
+  public async serialize(): Promise<ArrayBuffer> {
+    let sessionKeySequence = null;
+    if (this.sessionKey) {
+      const sessionPublicKeySerialized = await derSerializePublicKey(this.sessionKey.publicKey);
+      sessionKeySequence = makeImplicitlyTaggedSequence(
+        new OctetString({ valueHex: bufferToArray(this.sessionKey.keyId) }),
+        new OctetString({ valueHex: bufferToArray(sessionPublicKeySerialized) }),
+      );
+    }
+
     return makeImplicitlyTaggedSequence(
       new OctetString({ valueHex: this.privateNodeCertificate.serialize() }),
       new OctetString({ valueHex: this.gatewayCertificate.serialize() }),
+      ...(sessionKeySequence ? [sessionKeySequence] : []),
     ).toBER();
   }
+}
+
+async function deserializeSessionKey(sessionKeySequence: any): Promise<SessionKey | null> {
+  if (!sessionKeySequence) {
+    return null;
+  }
+  if (sessionKeySequence.valueBlock.value.length < 2) {
+    throw new InvalidMessageError('Session key SEQUENCE should have at least 2 items');
+  }
+  const sessionPublicKeyASN1 = sessionKeySequence.valueBlock.value[1] as Primitive;
+  const sessionKeyIdASN1 = sessionKeySequence.valueBlock.value[0] as Primitive;
+  let sessionPublicKey: CryptoKey;
+  try {
+    sessionPublicKey = await derDeserializeECDHPublicKey(sessionPublicKeyASN1.valueBlock.valueHex);
+  } catch (err) {
+    throw new InvalidMessageError(
+      new Error(err), // The original error could be a string 🤦
+      'Session key is not a valid ECDH public key',
+    );
+  }
+  return {
+    keyId: Buffer.from(sessionKeyIdASN1.valueBlock.valueHex),
+    publicKey: sessionPublicKey,
+  };
 }
