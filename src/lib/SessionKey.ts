b89843d27733c92b81931042e2c866a4f5b246e9
Gus Narea <gnarea@users.noreply.github.com>
2021-11-15 11:46:18 +0000

    fix: Move session key generation to new SessionKeypair class

--- src/lib/SessionKey.ts
+++ src/lib/SessionKey.ts
@@ -1,24 +1,5 @@
-import { generateRandom64BitValue } from './crypto_wrappers/_utils';
-import { generateECDHKeyPair } from './crypto_wrappers/keys';
-
 /** Key of the sender or recipient of the EnvelopedData value using the Channel Session Protocol */
-export class SessionKey {
-  /**
-   * Generate a new session key.
-   */
-  public static async generate(): Promise<{
-    readonly sessionKey: SessionKey;
-    readonly privateKey: CryptoKey;
-  }> {
-    const keyPair = await generateECDHKeyPair();
-    const keyId = await generateRandom64BitValue();
-    const sessionKey = new SessionKey(Buffer.from(keyId), keyPair.publicKey);
-    return { sessionKey, privateKey: keyPair.privateKey };
-  }
-
-  /**
-   * @param keyId Id of the ECDH key pair
-   * @param publicKey Public key of the (EC)DH key pair
-   */
-  constructor(public readonly keyId: Buffer, public readonly publicKey: CryptoKey) {}
+export interface SessionKey {
+  readonly keyId: Buffer;
+  readonly publicKey: CryptoKey;
 }
--- src/lib/SessionKeyPair.spec.ts
+++ src/lib/SessionKeyPair.spec.ts
@@ -1,23 +1,23 @@
 import { derSerializePublicKey } from './crypto_wrappers/keys';
-import { SessionKey } from './SessionKey';
+import { SessionKeyPair } from './SessionKeyPair';
 
 describe('generate', () => {
   test('keyId should be randomly generated, 64-bit value', async () => {
-    const { sessionKey } = await SessionKey.generate();
+    const { sessionKey } = await SessionKeyPair.generate();
 
     expect(sessionKey.keyId).toBeInstanceOf(Buffer);
     expect(sessionKey.keyId.byteLength).toEqual(8);
   });
 
   test('publicKey should be output', async () => {
-    const { sessionKey } = await SessionKey.generate();
+    const { sessionKey } = await SessionKeyPair.generate();
 
     expect(sessionKey.publicKey.type).toEqual('public');
     expect(sessionKey.publicKey.algorithm.name).toEqual('ECDH');
   });
 
   test('privateKey should correspond to public key', async () => {
-    const { sessionKey, privateKey } = await SessionKey.generate();
+    const { sessionKey, privateKey } = await SessionKeyPair.generate();
 
     expect(privateKey.type).toEqual('private');
     await expect(derSerializePublicKey(privateKey)).resolves.toEqual(
