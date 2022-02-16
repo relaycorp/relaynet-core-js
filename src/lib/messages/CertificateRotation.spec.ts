eb0fa1d9941aa4ac675b695df5fb8d5db98dfaa6
Gus Narea <gnarea@users.noreply.github.com>
2022-02-16 17:53:42 +0000

    feat(messages): Implement CertificateRotation
    Per https://specs.awala.network/RS-002#certificate-rotation

---
+++ src/lib/messages/CertificateRotation.spec.ts
@@ -0,0 +1,21 @@
+describe('CertificateRotation', () => {
+  describe('serialize', () => {
+    test.todo('Serialization should start with format signature', async () => {
+
+    });
+
+    test.todo('GCR should be serialized as a 1-item sequence');
+
+    test.todo('Certificate should be in GCR sequence');
+  });
+
+  describe('deserialize', () => {
+    test.todo('Serialization should start with format signature');
+
+    test.todo('Serialization should contain a sequence of a least 1 item');
+
+    test.todo('Malformed certificate should be refused');
+
+    test.todo('A new instance should be returned if serialization is valid');
+  });
+});
