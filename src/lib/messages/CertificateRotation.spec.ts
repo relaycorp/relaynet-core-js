22b316df76c69c5d1cfc052deda959f8691718cf
Gus Narea <gnarea@users.noreply.github.com>
2022-02-17 11:39:20 +0000

    implement serialization

--- src/lib/messages/CertificateRotation.spec.ts
+++ src/lib/messages/CertificateRotation.spec.ts
@@ -1,12 +1,75 @@
-describe('CertificateRotation', () => {
-  describe('serialize', () => {
-    test.todo('Serialization should start with format signature', async () => {
+import { Constructed, Primitive, Sequence } from 'asn1js';
 
+import { generateStubCert } from '../_test_utils';
+import { derDeserialize } from '../crypto_wrappers/_utils';
+import Certificate from '../crypto_wrappers/x509/Certificate';
+import { CertificateRotation } from './CertificateRotation';
+
+describe('CertificateRotation', () => {
+  let subjectCertificate: Certificate;
+  let issuerCertificate: Certificate;
+  beforeAll(async () => {
+    subjectCertificate = await generateStubCert();
+    issuerCertificate = await generateStubCert();
+  });
+
+  describe('serialize', () => {
+    test('Serialization should start with format signature', async () => {
+      const rotation = new CertificateRotation(subjectCertificate, [issuerCertificate]);
+
+      const serialization = rotation.serialize();
+
+      const expectedFormatSignature = Buffer.concat([
+        Buffer.from('Relaynet'),
+        Buffer.from([0x10, 0x00]),
+      ]);
+      expect(Buffer.from(serialization).slice(0, 10)).toEqual(expectedFormatSignature);
     });
 
-    test.todo('GCR should be serialized as a 1-item sequence');
+    test('Serialization should contain a 2-item sequence', async () => {
+      const rotation = new CertificateRotation(subjectCertificate, [issuerCertificate]);
 
-    test.todo('Certificate should be in GCR sequence');
+      const serialization = rotation.serialize();
+
+      const sequence = parseRotation(serialization);
+      expect(sequence.valueBlock.value).toHaveLength(2);
+    });
+
+    test('Subject certificate should be in sequence', async () => {
+      const rotation = new CertificateRotation(subjectCertificate, [issuerCertificate]);
+
+      const serialization = rotation.serialize();
+
+      const sequence = parseRotation(serialization);
+      const subjectCertificateASN1 = sequence.valueBlock.value[0];
+      const certificate = Certificate.deserialize(
+        (subjectCertificateASN1 as Primitive).valueBlock.valueHex,
+      );
+      expect(subjectCertificate.isEqual(certificate)).toBeTrue();
+    });
+
+    test('Chain certificates should be in sequence', async () => {
+      const rotation = new CertificateRotation(subjectCertificate, [issuerCertificate]);
+
+      const serialization = rotation.serialize();
+
+      const sequence = parseRotation(serialization);
+      const chainASN1 = sequence.valueBlock.value[1];
+      expect(chainASN1).toBeInstanceOf(Constructed);
+      const chainCertsSerialized = (chainASN1 as Constructed).valueBlock.value.map(
+        (c) => (c as Primitive).valueBlock.valueHex,
+      );
+      expect(chainCertsSerialized).toHaveLength(1);
+      const certificate = Certificate.deserialize(chainCertsSerialized[0]);
+      expect(issuerCertificate.isEqual(certificate)).toBeTrue();
+    });
+
+    function parseRotation(serialization: ArrayBuffer): Sequence {
+      const sequenceSerialized = serialization.slice(10);
+      const sequence = derDeserialize(sequenceSerialized);
+      expect(sequence).toBeInstanceOf(Sequence);
+      return sequence as Sequence;
+    }
   });
 
   describe('deserialize', () => {
--- src/lib/messages/CertificateRotation.ts
+++ src/lib/messages/CertificateRotation.ts
@@ -1,1 +1,35 @@
-export class CertificateRotation {}
+import { Constructed, OctetString } from 'asn1js';
+
+import { makeImplicitlyTaggedSequence } from '../asn1';
+import Certificate from '../crypto_wrappers/x509/Certificate';
+import { generateFormatSignature } from './formatSignature';
+
+export class CertificateRotation {
+  public static readonly FORMAT_SIGNATURE = generateFormatSignature(0x10, 0);
+
+  constructor(
+    public readonly subjectCertificate: Certificate,
+    public readonly chain: readonly Certificate[],
+  ) {}
+
+  public serialize(): ArrayBuffer {
+    // Serialize sequence
+    const chainASN1 = this.chain.map((c) => new OctetString({ valueHex: c.serialize() }));
+    const sequenceSerialized = makeImplicitlyTaggedSequence(
+      new OctetString({ valueHex: this.subjectCertificate.serialize() }),
+      new Constructed({ value: chainASN1 } as any),
+    ).toBER();
+
+    // Serialize entire message
+    const serialization = new ArrayBuffer(
+      CertificateRotation.FORMAT_SIGNATURE.byteLength + sequenceSerialized.byteLength,
+    );
+    const serializationView = new Uint8Array(serialization);
+    serializationView.set(CertificateRotation.FORMAT_SIGNATURE, 0);
+    serializationView.set(
+      new Uint8Array(sequenceSerialized),
+      CertificateRotation.FORMAT_SIGNATURE.byteLength,
+    );
+    return serialization;
+  }
+}
