e131b31228183358dbdd6ecc5dc6ddaba81c4d5c
Gus Narea <gnarea@users.noreply.github.com>
2022-03-07 11:07:57 +0100

    implement PrivateGateway

--- src/lib/nodes/PrivateGateway.ts
+++ src/lib/nodes/PrivateGateway.ts
@@ -1,10 +1,38 @@
+import { addDays, subMinutes } from 'date-fns';
+
+import {
+  getPrivateAddressFromIdentityKey,
+  getRSAPublicKeyFromPrivate,
+} from '../crypto_wrappers/keys';
+import Certificate from '../crypto_wrappers/x509/Certificate';
+import { issueGatewayCertificate } from '../pki';
 import { Gateway } from './Gateway';
 
 export class PrivateGateway extends Gateway {
-  public async generateCCA(
-    publicGatewayPrivateAddress: string,
-    publicGatewayPublicAddress: string,
-  ): Promise<ArrayBuffer> {
-    throw new Error('implement' + publicGatewayPrivateAddress + publicGatewayPublicAddress);
+  public async getOrCreateCDAIssuer(): Promise<Certificate> {
+    const now = new Date();
+
+    const publicKey = await getRSAPublicKeyFromPrivate(this.privateKey);
+    const privateAddress = await getPrivateAddressFromIdentityKey(publicKey);
+
+    const existingIssuer = await this.keyStores.certificateStore.retrieveLatest(
+      privateAddress,
+      privateAddress,
+    );
+    if (existingIssuer) {
+      const minExpiryDate = addDays(now, 90);
+      if (minExpiryDate <= existingIssuer.expiryDate) {
+        return existingIssuer;
+      }
+    }
+
+    const issuer = await issueGatewayCertificate({
+      issuerPrivateKey: this.privateKey,
+      subjectPublicKey: publicKey,
+      validityEndDate: addDays(now, 180),
+      validityStartDate: subMinutes(now, 90),
+    });
+    await this.keyStores.certificateStore.save(issuer, privateAddress);
+    return issuer;
   }
 }
